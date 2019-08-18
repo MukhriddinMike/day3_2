879f1f23d5b1eca90dbf052b23ab22f0d341e35a
MukhriddinMike <miwady@mail.ru>
2019-08-18 16:02:57 +0900

    Add project files.

---
+++ day3_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World";
+            s = s.Replace("World", "Universe");
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+
+        }
+    }
+}
